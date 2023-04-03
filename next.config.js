608ef418285d827557119e2fc410004adfc05f6b
Murf-y <charbelfayad64@gmail.com>
2023-04-03 21:03:38 +0300

    feat: edit next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const boundleAnalyzer = require('@next/bundle-analyzer')
 
-module.exports = nextConfig
+const withBundleAnalyzer = boundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer({
+  i18n: {
+    locales: ['en', 'fr'],
+    defaultLocale: 'en',
+  },
+  images: {
+    domains: [
+      // TODO ADD S3 BUCKETs here
+      'localhost',
+    ],
+    minimumCacheTTL: 604800, // 3600 * 24 * 7 = 1 week
+  },
+  env: {
+    API_URL: process.env.API_URL || 'http://localhost:3200/api',
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL || 'http://localhost:3000',
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET || 'secret',
+  },
+})
