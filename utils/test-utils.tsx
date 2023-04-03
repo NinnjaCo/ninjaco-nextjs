71840fbcb252699bca80b8b92d1cd1caa6f8a38c
Murf-y <charbelfayad64@gmail.com>
2023-04-03 21:15:20 +0300

    feat: add test script to run pre tests to setup provider (translations)

---
+++ utils/test-utils.tsx
@@ -0,0 +1,23 @@
+import { MemoryRouterProvider } from 'next-router-mock/MemoryRouterProvider'
+import { TranslationsProvider } from '@/contexts/translationContext'
+import { render } from '@testing-library/react'
+
+// Add in any providers here if necessary:
+const Providers = ({ children }) => {
+  // TranslateProvider
+  // MemoryRouterProvider (for links to affect the mock router)
+
+  return (
+    <MemoryRouterProvider>
+      <TranslationsProvider initialLocale="en">{children}</TranslationsProvider>
+    </MemoryRouterProvider>
+  )
+}
+
+const customRender = (ui, options = {}) => render(ui, { wrapper: Providers, ...options })
+
+// re-export everything
+export * from '@testing-library/react'
+
+// override render method
+export { customRender as render }
