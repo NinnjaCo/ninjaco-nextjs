124782b6eb3d8247686acef9222a38c5273f367a
Murf-y <charbelfayad64@gmail.com>
2023-04-03 21:24:20 +0300

    feat: add font + translation provider to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
+import { TranslationsProvider } from '@/contexts/TranslationContext'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Quicksand } from 'next/font/google'
+
+// TODO: add cookies script
+// TODO: add session manager
+// TODO: add Reactquery provider and hydration
+// TODO: add progress bar on top using NPProgress
+
+const quick_sand = Quicksand({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <TranslationsProvider initialLocale={pageProps.initialLocale}>
+      <main className={`${quick_sand.variable} font-sans`}>
+        <Component {...pageProps} />
+      </main>
+    </TranslationsProvider>
+  )
 }
