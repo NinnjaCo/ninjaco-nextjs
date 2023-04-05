d63dfaeea64e683c96dbe2b1c96666e170961d0c
Raghidkh <raghidkhoury2@gmail.com>
2023-04-05 17:23:28 +0300

    feat: faq page translation files

--- locales/en/index.tsx
+++ locales/en/index.tsx
@@ -41,5 +41,21 @@ const englishTranslations: WebsiteTranslations = {
         "Ninja Co has established over 20 education centers across Lebanon, offering an engaging and interactive curriculum in robotics that is constantly updated. The company's traditional in-person class model has proven successful, but to expand its reach and drive growth, We developed this online platform that will transform the company's offerings",
     },
   },
+  Faq: {
+    headTitle: 'Frequently Asked Questions',
+    question1: 'Should i have previous knowledge in programming?',
+    answer1:
+      'No, you do not need to have previous knowledge in programming to learn coding using visual blocks. This approach is designed to be beginner-friendly and accessible to students with no prior coding experience.',
+    question2: 'Do i need hardware parts?',
+    answer2:
+      'Well depends, for Arduino courses you will need some Arduino parts depending on the course',
+    question3: 'Should i install any other software to connect to the Arduino?',
+    answer3:
+      'No, you do not need to install any additional software to connect to the Arduino if the website or platform you are using provides a built-in feature to connect and program the Arduino with visual blocks. The website or platform should have instructions on how to connect the Arduino and run your code directly without requiring any external software.',
+    question4: 'Where can i register for the courses?',
+    answer4:
+      "You should be able to register for the courses on the website that offers the coding courses using visual blocks and Arduino. Look for a Register or Sign up button/link on the website's homepage or navigation menu. Clicking on that button/link should take you to a registration page where you can provide your information and enroll in the courses.",
+  },
 }
+
 export default englishTranslations
--- locales/fr/index.tsx
+++ locales/fr/index.tsx
@@ -42,6 +42,21 @@ const frenchTranslations: WebsiteTranslations = {
         "Ninja Co a établi plus de 20 centres d'éducation à travers le Liban, offrant un programme attractif et interactif en robotique constamment mis à jour. Le modèle en préseniel actuellement adopté par l'entreprise s'est avéré être un succès, mais pour étendre sa portée et stimuler sa croissance, nous avons développé cette plateforme en ligne qui transformera les offres disponibles.",
     },
   },
+  Faq: {
+    headTitle: 'Questions Fréquemment Posées',
+    question1: 'Dois-je avoir des connaissances préalables en programmation ?',
+    answer1:
+      "Non, vous n'avez pas besoin d'avoir des connaissances préalables en programmation pour apprendre à coder à l'aide de blocs visuels. Cette approche est conçue pour être conviviale pour les débutants et accessible aux étudiants sans expérience préalable en codage.",
+    question2: 'Ai-je besoin de pièces de quincaillerie ?',
+    answer2:
+      'Cela dépend, pour les cours Arduino, vous aurez besoin de certaines pièces Arduino en fonction du cours',
+    question3: "Dois-je installer un autre logiciel pour me connecter à l'Arduino ?",
+    answer3:
+      "Non, vous n'avez pas besoin d'installer de logiciel supplémentaire pour vous connecter à l'Arduino si le site Web ou la plate-forme que vous utilisez fournit une fonctionnalité intégrée pour connecter et programmer l'Arduino avec des blocs visuels. Le site Web ou la plate-forme doit contenir des instructions sur la façon de connecter l'Arduino et d'exécuter votre code directement sans nécessiter de logiciel externe",
+    question4: "Où puis-je m'inscrire aux cours?",
+    answer4:
+      "Vous devriez pouvoir vous inscrire aux cours sur le site Web qui propose les cours de codage utilisant des blocs visuels et Arduino. Recherchez un bouton/lien S'inscrire ou S'inscrire sur la page d'accueil ou le menu de navigation du site Web. En cliquant sur ce bouton/lien, vous serez redirigé vers une page d'inscription où vous pourrez fournir vos informations et vous inscrire aux cours.",
+  },
 }
 
 export default frenchTranslations
