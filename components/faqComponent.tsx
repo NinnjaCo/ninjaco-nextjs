c469050c26c6e047ed184cb5610a6c1911776e94
Raghidkh <raghidkhoury2@gmail.com>
2023-04-05 17:39:57 +0300

    feat: add footer to the fap page

--- components/faqComponent.tsx
+++ components/faqComponent.tsx
@@ -8,7 +8,7 @@ const FAQComponent: React.FC<{ question: string; answer: string }> = ({ question
     <div className=" mt-4 peer-open://#endregion">
       <button
         className={clsx(
-          'ml-14  w-3/4 px-4 py-2 text-left bg-brand-100 rounded-lg ',
+          ' w-3/4 mx-14 py-2 text-left bg-brand-100 rounded-lg ',
           isOpen && 'border-b-2 border-t-2 border-b-brand-400 border-t-brand-400',
           !isOpen && 'border-b-2 border-b-brand-300'
         )}
@@ -16,19 +16,19 @@ const FAQComponent: React.FC<{ question: string; answer: string }> = ({ question
       >
         <div className="flex flex-col gap-3">
           <div className="flex justify-between">
-            <div className="text-base md:text-lg lg:text-xl  text-brand font-medium">
+            <div className="text-base md:text-lg lg:text-xl ml-3 text-brand font-medium">
               {question}
             </div>
             <ChevronDownIcon
               className={clsx(
-                'w-5 h-5 text-brand transform transition-transform duration-200',
+                'w-5 h-5 mr-3 text-brand transform transition-transform duration-200',
                 isOpen && 'rotate-180'
               )}
             />
           </div>
           <div
             className={clsx(
-              'text-brand-400 text-sm md:text-base lg:text-lg font-medium',
+              'ml-3 text-brand-400 text-sm md:text-base lg:text-lg font-medium',
               isOpen && 'block',
               !isOpen && 'hidden'
             )}
--- pages/faq.tsx
+++ pages/faq.tsx
@@ -1,4 +1,5 @@
 import FAQComponent from '@/components/faqComponent'
+import Footer from '@/components/footer'
 import Head from 'next/head'
 import Menu from '@/components/menu'
 import useTranslation from '@/hooks/useTranslation'
@@ -35,13 +36,14 @@ export default function FAP_page() {
         <div className="mt-44 ml-14  font-mono text-lg md:text-xl lg:text-2xl font-bold text-brand">
           <h1>{t.Faq.headTitle}</h1>
         </div>
-        <div className="mt-10 flex flex-col gap-10">
+        <div className="mt-10 mb-24 flex flex-col gap-10">
           {/* map through faq array and display the three component */}
           {faq.map((item, index) => (
             <FAQComponent key={index} question={item.question} answer={item.answer} />
           ))}
         </div>
       </main>
+      <Footer />
     </>
   )
 }
