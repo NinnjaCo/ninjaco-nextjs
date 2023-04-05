7eabc1910f5223d0bd9a2b0b228fce1635e041ec
Raghidkh <raghidkhoury2@gmail.com>
2023-04-05 16:45:47 +0300

    feat: make faq button open in click

--- components/faqComponent.tsx
+++ components/faqComponent.tsx
@@ -5,27 +5,30 @@ import clsx from 'clsx'
 const FAQComponent: React.FC<{ question: string; answer: string }> = ({ question, answer }) => {
   const [isOpen, setIsOpen] = useState(false)
   return (
-    <div className="mt-4 peer-open://#endregion">
+    <div className=" mt-4 peer-open://#endregion">
       <button
-        className=" ml-14 flex justify-between w-3/4 px-4 py-2 text-left bg-brand-100 rounded-lg focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-opacity-50"
+        className={clsx(
+          'ml-14  w-3/4 px-4 py-2 text-left bg-brand-100 rounded-lg ',
+          isOpen && 'border-b-2 border-t-2 border-b-brand-400 border-t-brand-400',
+          !isOpen && 'border-b-2 border-b-brand-300'
+        )}
         onClick={() => setIsOpen(!isOpen)}
       >
-        <span className="text-lg font-medium text-brand-700">{question}</span>
-        <ChevronDownIcon
-          className={clsx(
-            'w-5 h-5 text-brand-700 transform transition-transform duration-200',
-            isOpen && 'rotate-180'
-          )}
-        />
+        <div className="flex flex-col gap-3">
+          <div className="flex justify-between">
+            <div className="text-lg font-medium text-brand-700">{question}</div>
+            <ChevronDownIcon
+              className={clsx(
+                'w-5 h-5 text-brand-700 transform transition-transform duration-200',
+                isOpen && 'rotate-180'
+              )}
+            />
+          </div>
+          <div className={clsx('text-brand-700', isOpen && 'block', !isOpen && 'hidden')}>
+            {answer}
+          </div>
+        </div>
       </button>
-      <div
-        className={clsx(
-          'w-3/4 pl-14 px-4 py-2 text-brand-700 transition-all duration-200',
-          isOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'
-        )}
-      >
-        {answer}
-      </div>
     </div>
   )
 }
