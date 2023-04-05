bcec4ecb2bb96f229b2649bb7481de793b50509b
tony123bs <toni123bousleiman@gmail.com>
2023-04-05 17:29:22 +0300

    feat: add translation

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -7,19 +7,20 @@ export default function FAP_page() {
   const t = useTranslation()
   const faq = [
     {
-      question: 'Should i have previous knowledge in programming?',
-      answer:
-        'No, you do not need to have previous knowledge in programming to learn coding using visual blocks. This approach is designed to be beginner-friendly and accessible to students with no prior coding experience.',
+      question: t.Faq.question1 as string,
+      answer: t.Faq.answer1 as string,
     },
     {
-      question: 'Do i need hardware parts?',
-      answer:
-        'Well depends, for Arduino courses you will need some Arduino parts depending on the course However for other type of courses, you do not need any external hardware parts!',
+      question: t.Faq.question2 as string,
+      answer: t.Faq.answer2 as string,
     },
     {
-      question: 'Should i install any other software to connect to the Arduino?',
-      answer:
-        'No, you do not need to install any additional software to connect to the Arduino if the website or platform you are using provides a built-in feature to connect and program the Arduino with visual blocks. The website or platform should have instructions on how to connect the Arduino and run your code directly without requiring any external software.',
+      question: t.Faq.question3 as string,
+      answer: t.Faq.answer3 as string,
+    },
+    {
+      question: t.Faq.question4 as string,
+      answer: t.Faq.answer4 as string,
     },
   ]
   return (
@@ -32,7 +33,7 @@ export default function FAP_page() {
       <main>
         <Menu isBackgroundLight={false} />
         <div className="mt-44 ml-14  font-mono text-lg md:text-xl lg:text-2xl font-bold text-brand">
-          <h1>Frequently Asked Questions</h1>
+          <h1>{t.Faq.headTitle}</h1>
         </div>
         <div className="mt-10 flex flex-col gap-10">
           {/* map through faq array and display the three component */}
