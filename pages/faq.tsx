8611cea4f19fa9a784587d5b6203ec2479136905
Raghidkh <raghidkhoury2@gmail.com>
2023-04-06 14:36:38 +0300

    fix: change component margin, reduce movile margin

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -33,18 +33,16 @@ export default function FAP_page() {
       </Head>
       <main>
         <Menu isBackgroundLight={false} />
-        <div className="mt-44 ml-14  font-mono text-lg md:text-xl lg:text-2xl font-bold text-brand">
+        <div className="mt-44 ml-7 md:ml-14  font-mono text-lg md:text-xl lg:text-2xl font-bold text-brand">
           <h1>{t.Faq.headTitle}</h1>
         </div>
-        <div className="mt-10 mb-24 flex flex-col gap-10">
+        <div className="px-7 md:px-14 mt-14 mb-24 flex flex-col gap-10">
           {/* map through faq array and display the three component */}
           {faq.map((item, index) => (
             <FAQComponent key={index} question={item.question} answer={item.answer} />
           ))}
         </div>
-        <div className="mt-44">
-          <Footer />
-        </div>
+        <div className="mt-44"></div>
       </main>
       <Footer />
     </>
