511ebee411f92d6d1a35f695f0e543398d57814b
tony123bs <toni123bousleiman@gmail.com>
2023-04-08 15:11:39 +0300

    feat: create admin user view :fire:

---
+++ components/admin/menuSection.tsx
@@ -0,0 +1,73 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import creators from '@/images/creators.svg'
+import dashboard_logo from '@/images/dashboard_logo.svg'
+import logo_white from '@/images/logo_white.svg'
+import logout from '@/images/logout.svg'
+import open_book from '@/images/open_book.svg'
+import profile_icon from '@/images/profile_icon.svg'
+import user_group from '@/images/user_group.svg'
+
+const MenuSection = () => {
+  const menuSection = [
+    {
+      icon: dashboard_logo,
+      text: 'DASHBOARD',
+    },
+    {
+      icon: open_book,
+      text: 'COURSES',
+    },
+    {
+      icon: user_group,
+      text: 'USERS',
+    },
+    {
+      icon: creators,
+      text: 'CREATORS',
+    },
+  ]
+
+  const profile_logout = [
+    {
+      icon: profile_icon,
+      text: 'PROFILE',
+    },
+    {
+      icon: logout,
+      text: 'LOGOUT',
+    },
+  ]
+  return (
+    <div className=" bg-brand h-screen w-1/5 flex flex-col justify-between items-center">
+      <Link href={'/'}>
+        <Image src={logo_white} alt="Hero Image" width={150} height={50} className=" mt-7"></Image>
+      </Link>
+      <div className="relative flex flex-col gap-3 w-2/3 ">
+        {menuSection.map((item) => (
+          <>
+            <div className="bg-brand-300 h-px opacity-25" />
+            <div key={item.text} className="flex gap-3 mb-4">
+              <Image src={item.icon} alt="image"></Image>
+              <button className="text-brand-300 font-semibold text-sm">{item.text}</button>
+            </div>
+          </>
+        ))}
+      </div>
+      <div className="relative flex flex-col gap-3 w-2/3 ">
+        {profile_logout.map((item) => (
+          <>
+            <div className="bg-brand-300  h-px opacity-25" />
+            <div key={item.text} className="flex gap-3 mb-4">
+              <Image src={item.icon} alt="image"></Image>
+              <button className="text-brand-300 font-semibold text-sm">{item.text}</button>
+            </div>
+          </>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default MenuSection
---
+++ pages/admin/users.tsx
@@ -0,0 +1,92 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import MenuSection from '@/components/admin/menuSection'
+import React, { Fragment } from 'react'
+
+export default function AdminUserView() {
+  return (
+    <>
+      <Head>
+        <title>Ninja Co | Admin Users View</title>
+        <meta name="description" content="Leading online platform for visual programming" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main className="flex h-screen">
+        <MenuSection />
+
+        {/* leaderboard */}
+
+        <div className="relative overflow-x-auto h-fit">
+          <table className="w-5/6 ml-24 text-sm text-left text-brand rounded-t-2xl">
+            <thead className="text-brand w-max uppercase bg-brand-200 ">
+              <tr>
+                <th scope="col" className="px-6 py-3">
+                  ID
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  First Name
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Last Name
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Email
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Date of Birth
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Createt At
+                </th>
+                <th scope="col" className="px-6 py-3"></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="bg-brand-50">
+                <th scope="row" className="px-6 py-4  text-brand">
+                  <td className=" py-4">1</td>
+                </th>
+
+                <td className="px-6 py-4">Raghid</td>
+                <td className="px-6 py-4">khoury</td>
+                <td className="px-6 py-4">raghid@gmail.commmm</td>
+                <td className="px-6 py-4">1/1/1000</td>
+                <td className="px-6 py-4">20/2/2003</td>
+              </tr>
+              <tr className="bg-brand-50">
+                <th scope="row" className="px-6 py-4  text-brand">
+                  <td className=" py-4">2</td>
+                </th>
+                <td className="px-6 py-4">Raghid</td>
+                <td className="px-6 py-4">khoury</td>
+                <td className="px-6 py-4">raghid@gmail.commmm</td>
+                <td className="px-6 py-4">1/1/1000</td>
+                <td className="px-6 py-4">20/2/2003</td>
+              </tr>
+              <tr className="bg-brand-50">
+                <th scope="row" className="px-6 py-4  text-brand">
+                  <td className=" py-4">3</td>
+                </th>
+                <td className="px-6 py-4">Raghid</td>
+                <td className="px-6 py-4">khoury</td>
+                <td className="px-6 py-4">raghid@gmail.commmm</td>
+                <td className="px-6 py-4">1/1/1000</td>
+                <td className="px-6 py-4">20/2/2003</td>
+              </tr>
+              <tr className="bg-brand-50">
+                <th scope="row" className="px-6 py-4  text-brand">
+                  <td className=" py-4">4</td>
+                </th>
+                <td className="px-6 py-4">Raghid</td>
+                <td className="px-6 py-4">khoury</td>
+                <td className="px-6 py-4">raghid@gmail.commmm</td>
+                <td className="px-6 py-4">1/1/1000</td>
+                <td className="px-6 py-4">20/2/2003</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </main>
+    </>
+  )
+}
