da68dcf3ec093f465ee259e5f0134626b8a8592b
tony123bs <toni123bousleiman@gmail.com>
2023-04-08 23:28:28 +0300

    refactor: change date format from ISO to string date

--- pages/admin/users.tsx
+++ pages/admin/users.tsx
@@ -1,15 +1,15 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import MenuSection from '@/components/admin/menuSection'
-import React, { Fragment, useEffect } from 'react'
-import filter_logo from '@/images/filter_logo.svg'
-
 import { ISODateString } from 'next-auth'
+
 import { User } from '@/models/crud/user.model'
 import { UserApi } from '@/utils/api/user'
 import { date } from 'yup'
 import { getSession, useSession } from 'next-auth/react'
+import Head from 'next/head'
+import Image from 'next/image'
+import MenuSection from '@/components/admin/menuSection'
+import React, { Fragment, useEffect } from 'react'
 import dayjs from 'dayjs'
+import filter_logo from '@/images/filter_logo.svg'
 import format from 'date-fns/format'
 import pen_logo from '@/images/pen_logo.svg'
 
@@ -18,6 +18,13 @@ const AdminUserView: React.FC<{ users: User[] }> = ({ users }) => {
   //   const users = api.find()
   // use the same approach but in useEffect
   console.log(users[0].firstName)
+
+  function newDate(d: string): React.ReactNode {
+    const date = new Date(d)
+    const formattedDate = format(date, 'dd/MM/yyyy')
+
+    return formattedDate
+  }
 
   return (
     <>
@@ -33,18 +40,18 @@ const AdminUserView: React.FC<{ users: User[] }> = ({ users }) => {
         <div className="flex flex-col ">
           <div className="flex justify justify-between">
             <div className="text-brand-700 text-3xl font-semibold ml-24 mt-10">users</div>
-            <div className="mt-10 mr-14 btn w-32 h-10 rounded-xl border-x-2 border-y-2 border-brand text-lg font-semibold text-brand   ">
+            <div className="mt-10 mr-24 btn w-32 h-10 rounded-xl border-x-2 border-y-2 border-brand text-lg font-semibold text-brand   ">
               Add user
             </div>
           </div>
-          <div className="ml-24 mt-1 text-sm text-brand "> 1234 entries found</div>
+          <div className="ml-24 mt-1 text-sm text-brand  "> 1234 entries found</div>
           <div className="ml-24 mt-10 mb-2 btn w-32 h-8 rounded-lg border-x-1 border-y-1 bg-brand-200 border-brand text-lg font-semibold text-brand flex gap-2 max-w-fit">
             {/* add filter_logo image */}
             <Image src={filter_logo} alt={'filter'}></Image>
             Filter
           </div>
-          <div className="relative overflow-x-auto h-fit mb-16">
-            <table className="w-5/6 ml-24 text-sm text-left text-brand rounded-t-2xl ">
+          <div className="  max-w-fit relative overflow-x-auto mb-16 flex ">
+            <table className=" mx-24 text-sm text-left text-brand rounded-t-2xl  ">
               <thead className="text-brand w-max uppercase bg-brand-200 ">
                 <tr>
                   <th scope="col" className="px-6 py-3">
@@ -76,8 +83,8 @@ const AdminUserView: React.FC<{ users: User[] }> = ({ users }) => {
                       <td className="px-6 py-4">{users[index].firstName}</td>
                       <td className="px-6 py-4">{users[index].lastName}</td>
                       <td className="px-6 py-4">{users[index].email}</td>
-                      <td className="px-6 py-4">{users[index].dateOfBirth}</td>
-                      <td className="px-6 py-4">{users[index].createdAt}</td>
+                      <td className="px-6 py-4">{newDate(users[index].dateOfBirth)}</td>
+                      <td className="px-6 py-4">{newDate(users[index].createdAt)}</td>
                       <td className="px-6 py-4">
                         <button className="bg-brand-200 rounded-full w-6 h-6">
                           <Image className="rounded" src={pen_logo} alt={'pen logo'}></Image>
