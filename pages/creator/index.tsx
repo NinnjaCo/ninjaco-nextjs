18dcfbe7e5b58b031539a719bfe777ef3d93d65b
tony123bs <toni123bousleiman@gmail.com>
2023-04-11 18:06:06 +0300

    feat: make creator courses page header design responsive

--- pages/creator/index.tsx
+++ pages/creator/index.tsx
@@ -16,18 +16,20 @@ export default function Home() {
 
         <div className="flex flex-row mt-32 justify-between">
           <div className="flex flex-col ml-14 gap-6">
-            <div className="text-brand-700 font-semibold text-2xl ">courses</div>
+            <div className="text-brand-700 font-semibold  text-base md:text-xl lg:text-2xl ">
+              Courses
+            </div>
             <div className="flex flex-row gap-10 justify-center">
-              <div className="text-lg text-brand"> 210 enties</div>
+              <div className="text-sm md:text-base lg:text-lg text-brand"> 210 enties</div>
 
-              <button className="bg-brand-200 flex items-center  pl-2 gap-3 hover:bg-brand-400 text-lg font-semibold w-28 rounded-md text-brand-500 border-x-2 border-y-2 border-brand-700">
-                <Image src={filter} alt="Hero Image " width={20}></Image>
+              <button className="bg-brand-200 flex items-center  pl-2 gap-3 hover:bg-brand-400 text-sm md:text-base lg:text-lg font-semibold w-20 md:w-24 lg:w-28 rounded-md text-brand-500 border-x-2 border-y-2 border-brand-700">
+                <Image src={filter} alt="Hero Image " width={17}></Image>
                 Filter
               </button>
             </div>
           </div>
           <div className="text-brand-700 font-semibold text-3xl mr-12 ">
-            <button className="bg-secondary hover:bg-secondary-300 text-xl font-semibold w-40 h-10 rounded-xl text-brand-700 border-x-2 border-y-2 border-brand-700">
+            <button className="bg-secondary hover:bg-secondary-300 text-base md:text-lg lg:text-xl font-semibold w-28 h-8 md:w-32 md:h-9 lg:w-40 lg:h-10 rounded-xl text-brand-700 border-x-2 border-y-2 border-brand-700">
               Create course
             </button>
           </div>
