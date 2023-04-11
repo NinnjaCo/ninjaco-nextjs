521da773cd123f89556066a98e21ee49acf27f85
Raghidkh <raghidkhoury2@gmail.com>
2023-04-11 19:09:30 +0300

    feat: add creators dashboard buttons

--- pages/creator/index.tsx
+++ pages/creator/index.tsx
@@ -6,6 +6,8 @@ import { use } from 'react'
 import CoursesComponent from '@/components/creator/coursesComponent'
 import CreatorMenu from '@/components/creator/creatorMenu'
 import Head from 'next/head'
+import Image from 'next/image'
+import filter from '@/images/filterIcon.svg'
 
 export default function Home({ user }: { user: User }) {
   return (
@@ -17,8 +19,26 @@ export default function Home({ user }: { user: User }) {
       </Head>
       <main className="relative w-full">
         <CreatorMenu isOnCoursePage={true} creator={user} />
-
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 w-full gap-8 items-center mt-12 px-10 place-items-center">
+        <div className="flex flex-row mt-7 justify-between">
+          <div className="flex flex-col ml-14 gap-6">
+            <div className="text-brand-700 font-semibold  text-sm md:text-base lg:text-xl ">
+              Courses
+            </div>
+            <div className="flex flex-row gap-10 justify-center">
+              <div className="text-xs md:text-sm lg:text-base text-brand"> 210 enties</div>{' '}
+              <button className="bg-brand-200 flex items-center  pl-2 gap-3 hover:bg-brand-400 text-xs md:text-sm lg:text-base font-semibold w-20 md:w-24 lg:w-28 rounded-md text-brand-500 border-x-2 border-y-2 border-brand-700">
+                <Image src={filter} alt="Hero Image " width={17}></Image>
+                Filter
+              </button>
+            </div>
+          </div>
+          <div className="text-brand-700 font-semibold  mr-12 ">
+            <button className="bg-secondary hover:bg-secondary-300 text-base lg:text-lg font-semibold w-28 h-8 md:w-32 md:h-9 lg:w-40 lg:h-10 rounded-xl text-brand-700 border-x-2 border-y-2 border-brand-700">
+              Create course
+            </button>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 w-full gap-8 items-center mt-7 px-10 place-items-center">
           <div>
             <CoursesComponent
               image={
