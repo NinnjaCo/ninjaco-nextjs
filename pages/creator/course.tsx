f05f327f33e9a957ae6827e6bcef6910ffa6425d
Raghidkh <raghidkhoury2@gmail.com>
2023-04-13 01:56:32 +0300

    feat: add menu to view courses page

---
+++ pages/creator/course.tsx
@@ -0,0 +1,59 @@
+import { User } from '@/models/crud'
+import { UserApi } from '@/utils/api/user'
+import { authOptions } from '../api/auth/[...nextauth]'
+import { getServerSession } from 'next-auth'
+import CreatorMenu from '@/components/creator/creatorMenu'
+import Head from 'next/head'
+
+export default function Course({ user }: { user: User }) {
+  return (
+    <>
+      <Head>
+        <title>NinjaCo | Course</title>
+        <meta name="description" content="Leading online platform for visual programming" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main className="relative h-screen w-full">
+        <CreatorMenu
+          {...{
+            isOnCoursePage: true,
+            creator: user,
+          }}
+        />
+      </main>
+    </>
+  )
+}
+
+export const getServerSideProps = async (context) => {
+  const { query, req, res } = context
+
+  const session = await getServerSession(req, res, authOptions)
+  if (!session) {
+    return {
+      redirect: {
+        destination:
+          (query.redirectTo as string | undefined) || '/auth/signup?error=Token%20Expired',
+        permanent: false,
+      },
+    }
+  }
+
+  const response = await new UserApi(session).findOne(session.id)
+  if (!response || !response.payload) {
+    return {
+      props: {
+        redirect: {
+          destination: '/auth/signin',
+          permanent: false,
+        },
+      },
+    }
+  }
+
+  return {
+    props: {
+      user: response.payload,
+    },
+  }
+}
