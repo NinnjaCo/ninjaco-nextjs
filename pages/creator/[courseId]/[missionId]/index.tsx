5c4e0f318b3abbd7a8cf5d1f0558affe5fcac7c4
Murf-y <charbelfayad64@gmail.com>
2023-04-15 01:28:09 +0300

    feat: mission view page link it to backend

--- pages/creator/[courseId]/[missionId]/index.tsx
+++ pages/creator/[courseId]/[missionId]/index.tsx
@@ -1,3 +1,8 @@
+import { Category } from '@/models/crud/category.model'
+import { CategoryApi } from '@/utils/api/category/category.api'
+import { Course } from '@/models/crud/course.model'
+import { CourseApi } from '@/utils/api/course/course.api'
+import { Mission } from '@/models/crud/mission.model'
 import { User } from '@/models/crud'
 import { UserApi } from '@/utils/api/user'
 import { authOptions } from '@/pages/api/auth/[...nextauth]'
@@ -8,9 +13,17 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 
-const levels = Array.from({ length: 7 }, (_, i) => i + 1)
-
-export default function Course({ user }: { user: User }) {
+export default function MissionPage({
+  user,
+  mission,
+  course,
+  missionCategory,
+}: {
+  user: User
+  mission: Mission
+  course: Course
+  missionCategory: Category
+}) {
   return (
     <>
       <Head>
@@ -24,7 +37,7 @@ export default function Course({ user }: { user: User }) {
           <div className="w-52 h-32 relative">
             <Image
               className="bg-brand-100 border-2 border-brand-200 rounded-xl w-52 h-32"
-              src="https://s3-us-west-2.amazonaws.com/cherpa01-static/curriculum/courses/intro_robotics_electronics.png"
+              src={mission.image}
               style={{
                 objectFit: 'contain',
               }}
@@ -35,44 +48,49 @@ export default function Course({ user }: { user: User }) {
           </div>
           <div className="flex flex-col gap-9 w-full">
             <div className="flex justify-between gap-6 items-center">
-              <div className=" text-brand font-semibold text-xl md:text-3xl"> How it works</div>
+              <div className=" text-brand font-semibold text-xl md:text-3xl">{mission.title}</div>
               <button className="text-xs  md:text-base font-semibold btn btn-secondary bg-secondary rounded-lg md:rounded-xl text-brand-700 border-brand-700 hover:bg-secondary-800 h-fit">
                 Edit Mission
               </button>
             </div>
             <div className=" text-brand-500 font-medium text-xs md:text-base w-full">
-              This course will lorem ipsum lorem ipsum lorem ipsumThis course will lorem ipsum lorem
-              ipsum lorem ipsum
+              {mission.description}
             </div>
           </div>
         </div>
         <div className="flex gap-3 items-center w-full flex-wrap px-6 my-8">
           <div className=" text-brand font-medium text-xs md:text-base">Mission Category:</div>
-          <Chip text="Robotics" />
+          <Chip text={missionCategory.categoryName} />
         </div>
         <div className="flex flex-col px-6 pb-12 gap-6">
           <div className="flex justify-between gap-10">
             <div className="font-semibold text-2xl">Levels</div>
             <Link
               className=" text-xs md:text-base font-semibold btn btn-secondary bg-secondary rounded-lg md:rounded-xl text-brand-700 border-brand-700 hover:bg-secondary-800 h-fit"
-              href={'/creator/blabla/create'}
+              href={`/creator/${course._id}/${mission._id}/create`}
             >
               Add level
             </Link>
           </div>
 
-          <div className="grid grid-cols-4 sm:grid-cols-7 md:grid-cols-10 lg:grid-cols-11 xl:grid-cols-12 w-full gap-8 items-center place-items-center">
-            {levels.map((level, index) => (
-              <div
-                className={`rounded-full w-10 h-10 flex justify-center items-center text-center p-8 text-2xl font-semibold text-brand-800 ${
-                  level % 2 === 0 ? 'bg-secondary-300' : 'bg-brand-300'
-                }`}
-                key={level}
-              >
-                {level}
-              </div>
-            ))}
-          </div>
+          {mission.levels.length !== 0 ? (
+            <div className="grid grid-cols-4 sm:grid-cols-7 md:grid-cols-10 lg:grid-cols-11 xl:grid-cols-12 w-full gap-8 items-center place-items-center">
+              {mission.levels.map((level, index) => (
+                <div
+                  className={`rounded-full w-10 h-10 flex justify-center items-center text-center p-8 text-2xl font-semibold text-brand-800 ${
+                    level.levelNumber % 2 === 0 ? 'bg-secondary-300' : 'bg-brand-300'
+                  }`}
+                  key={index}
+                >
+                  {level.levelNumber}
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div className="text-brand-500 font-medium text-xs md:text-base w-full">
+              No levels yet
+            </div>
+          )}
         </div>
       </main>
     </>
@@ -81,6 +99,8 @@ export default function Course({ user }: { user: User }) {
 
 export const getServerSideProps = async (context) => {
   const { query, req, res } = context
+
+  const { courseId, missionId } = query
 
   const session = await getServerSession(req, res, authOptions)
   if (!session) {
@@ -105,9 +125,39 @@ export const getServerSideProps = async (context) => {
     }
   }
 
+  const course = await new CourseApi(session).findOne(courseId)
+
+  if (!course || !course.payload) {
+    return {
+      props: {
+        redirect: {
+          destination: '/creator',
+          permanent: false,
+        },
+      },
+    }
+  }
+  const mission = course.payload.missions.find((mission) => mission._id === missionId)
+
+  if (!mission) {
+    return {
+      props: {
+        redirect: {
+          destination: '/creator/' + courseId,
+          permanent: false,
+        },
+      },
+    }
+  }
+
+  const missionCategory = await new CategoryApi(session).findOne(mission.categoryId)
+
   return {
     props: {
       user: response.payload,
+      mission: mission,
+      course: course.payload,
+      missionCategory: missionCategory.payload,
     },
   }
 }
