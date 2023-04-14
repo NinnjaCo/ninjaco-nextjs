f7391b324dba2a8ecfe258c63081871eca18874c
Murf-y <charbelfayad64@gmail.com>
2023-04-14 22:47:06 +0300

    fix: course card

--- components/creator/courseCard.tsx
+++ components/creator/courseCard.tsx
@@ -1,29 +1,27 @@
+import { Course } from '@/models/crud/course.model'
 import Image from 'next/image'
 import profileIcon from '@/images/profileIcon.svg'
 import suitCase from '@/images/suitCase.svg'
 
-const CourseCard: React.FC<{ image: string; name: string; mission: string; age: string }> = ({
-  image,
-  name,
-  mission,
-  age,
-}) => {
+const CourseCard: React.FC<{ course: Course }> = ({ course }) => {
   return (
     <div className="bg-brand-50 w-fit h-fit rounded-lg p-2 flex flex-col gap-2 relative">
       <div className="flex flex-col gap-2">
         <div className="bg-brand-100 rounded-t-lg px-10">
-          <Image src={image} alt="courses" width={125} height={100} />
+          <Image src={course.image} alt="courses" width={125} height={100} />
         </div>
-        <div className="text-brand font-semibold text-sm">{name}</div>
+        <div className="text-brand font-semibold text-sm">{course.title}</div>
         <div className=" flex gap-2">
           <Image src={suitCase} alt="suitcase" />
-          <div className="text-brand-500 font-normal text-xs"> missions: </div>
-          <div className="text-brand-500 font-normal text-xs">{mission}</div>
+          <div className="text-brand-500 font-normal text-xs">missions:</div>
+          <div className="text-brand-500 font-normal text-xs">{10}</div>
         </div>
         <div className=" flex gap-2">
           <Image src={profileIcon} alt="suitcase" />
-          <div className="text-brand-500 font-normal text-xs"> age:</div>
-          <div className="text-brand-500 font-normal text-xs">{age}</div>
+          <div className="text-brand-500 font-normal text-xs">age:</div>
+          <div className="text-brand-500 font-normal text-xs">
+            {course.ageRange ? course.ageRange[0] : ''}
+          </div>
         </div>
       </div>
     </div>
