4f75c552c1bd8f5c5b236c65ce6aa733565b724a
Murf-y <charbelfayad64@gmail.com>
2023-04-15 01:58:49 +0300

    fix: game card responsivness

--- components/creator/gameCard.tsx
+++ components/creator/gameCard.tsx
@@ -5,8 +5,17 @@ const GameCard: React.FC<{ game: Game }> = ({ game }) => {
   return (
     <div className="bg-brand-50 w-fit h-fit rounded-lg p-2 flex flex-col gap-2 relative">
       <div className="flex flex-col gap-2">
-        <div className="bg-brand-100 rounded-t-lg px-10">
-          <Image src={game.image} alt="courses" width={125} height={100} />
+        <div className="bg-brand-100 rounded-t-lg px-10 w-44 h-32 relative">
+          <Image
+            className="bg-brand-200 border-2 border-brand-400 rounded w-52 h-32"
+            src={game.image}
+            style={{
+              objectFit: 'contain',
+            }}
+            fill
+            alt="PP"
+            priority
+          />
         </div>
         <div className="text-brand font-semibold text-sm">{game.title}</div>
       </div>
