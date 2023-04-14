4a3eeed18f4c014ed58be9476442ebabf6cf6a24
Murf-y <charbelfayad64@gmail.com>
2023-04-15 01:35:58 +0300

    fix: mission and course card responsivness

--- components/creator/courseCard.tsx
+++ components/creator/courseCard.tsx
@@ -6,10 +6,19 @@ import suitCase from '@/images/suitCase.svg'
 const CourseCard: React.FC<{ course: Course }> = ({ course }) => {
   console.log(course)
   return (
-    <div className="bg-brand-50 w-fit h-fit rounded-lg p-2 flex flex-col gap-2 relative">
-      <div className="flex flex-col gap-2">
-        <div className="bg-brand-100 rounded-t-lg px-10">
-          <Image src={course.image} alt="courses" width={125} height={100} />
+    <div className="bg-brand-50 w-fit h-fit rounded-lg p-2 flex flex-col gap-2 relative shadow-md">
+      <div className="flex flex-col gap-2 w-fit h-fit">
+        <div className="bg-brand-100 rounded-t-lg px-10 w-44 h-32 relative">
+          <Image
+            className="bg-brand-200 border-2 border-brand-400 rounded w-52 h-32"
+            src={course.image}
+            style={{
+              objectFit: 'contain',
+            }}
+            fill
+            alt="PP"
+            priority
+          />
         </div>
         <div className="text-brand font-semibold text-sm">{course.title}</div>
         <div className=" flex gap-2">
--- components/creator/missionCard.tsx
+++ components/creator/missionCard.tsx
@@ -4,10 +4,19 @@ import suitCase from '@/images/suitCase.svg'
 
 const MissionCard: React.FC<{ mission: Mission }> = ({ mission }) => {
   return (
-    <div className="bg-brand-50 w-fit h-fit rounded-lg p-2 flex flex-col gap-2 relative">
+    <div className="bg-brand-50 w-fit h-fit rounded-lg p-2 flex flex-col gap-2 relative shadow-md">
       <div className="flex flex-col gap-2">
-        <div className="bg-brand-100 rounded-t-lg px-10">
-          <Image src={mission.image} alt="courses" width={125} height={100} />
+        <div className="bg-brand-100 rounded-t-lg px-10 w-44 h-32 relative">
+          <Image
+            className="bg-brand-200 border-2 border-brand-400 rounded w-52 h-32"
+            src={mission.image}
+            style={{
+              objectFit: 'contain',
+            }}
+            fill
+            alt="PP"
+            priority
+          />
         </div>
         <div className="text-brand font-semibold text-sm">{mission.title}</div>
         <div className=" flex gap-2">
