59efc5fe9d59d314481064be4b7ac76d55f7b3dd
Murf-y <charbelfayad64@gmail.com>
2023-04-15 01:25:30 +0300

    feat: update mission card

--- components/creator/missionCard.tsx
+++ components/creator/missionCard.tsx
@@ -1,22 +1,19 @@
+import { Mission } from '@/models/crud/mission.model'
 import Image from 'next/image'
 import suitCase from '@/images/suitCase.svg'
 
-const MissionCard: React.FC<{ image: string; name: string; levels: string }> = ({
-  image,
-  name,
-  levels,
-}) => {
+const MissionCard: React.FC<{ mission: Mission }> = ({ mission }) => {
   return (
     <div className="bg-brand-50 w-fit h-fit rounded-lg p-2 flex flex-col gap-2 relative">
       <div className="flex flex-col gap-2">
         <div className="bg-brand-100 rounded-t-lg px-10">
-          <Image src={image} alt="courses" width={125} height={100} />
+          <Image src={mission.image} alt="courses" width={125} height={100} />
         </div>
-        <div className="text-brand font-semibold text-sm">{name}</div>
+        <div className="text-brand font-semibold text-sm">{mission.title}</div>
         <div className=" flex gap-2">
           <Image src={suitCase} alt="suitcase" />
           <div className="text-brand-500 font-normal text-xs"> levels: </div>
-          <div className="text-brand-500 font-normal text-xs">{levels}</div>
+          <div className="text-brand-500 font-normal text-xs">{mission.levels.length}</div>
         </div>
       </div>
     </div>
