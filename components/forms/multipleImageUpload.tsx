5a0cdb88714d3d580d39bd7c8a45b16d003fa5b8
Murf-y <charbelfayad64@gmail.com>
2023-04-15 17:45:23 +0300

    feat: allow multiImageUpload component to take initials images as default images

--- components/forms/multipleImageUpload.tsx
+++ components/forms/multipleImageUpload.tsx
@@ -12,6 +12,10 @@ interface MultipleImageUploadProps {
   error?: string
   label?: string
   rootClassName?: string
+  initialData?: {
+    initialImages: string[]
+    editInitialImages: (newImages: string[]) => void
+  }
 }
 
 const MultipleImageUpload = ({
@@ -21,9 +25,13 @@ const MultipleImageUpload = ({
   label,
   error,
   rootClassName,
+  initialData,
 }: MultipleImageUploadProps) => {
   // Use array because the library forces an array of ImageType
   const [uploadedPictures, setUploadedPictures] = useState<ImageListType>([])
+  const [initialsImage, setInitialImages] = useState<string[] | undefined>(
+    initialData?.initialImages
+  )
 
   const getImageUploadErrorMessage = (error: ErrorsType) => {
     if (error?.acceptType) {
@@ -163,6 +171,54 @@ const MultipleImageUpload = ({
                       )
                     )
                   })}
+                  {initialsImage &&
+                    initialsImage.map((image: string, index: number) => {
+                      return (
+                        image && (
+                          <div key={index} className="relative w-52 h-36 bg-brand-50 rounded">
+                            <Image
+                              src={image}
+                              placeholder="blur"
+                              blurDataURL={image}
+                              alt="Uploaded Image"
+                              style={{
+                                objectFit: 'contain',
+                              }}
+                              fill
+                              sizes="(max-width: 768px) 40vw,
+                            (max-width: 1200px) 50vw,
+                            60vw"
+                              className="rounded-md"
+                            />
+                            <button
+                              className="absolute top-1 left-1 p-1 rounded-full bg-error-dark text-white"
+                              onClick={() => {
+                                const newImages = initialsImage?.filter(
+                                  (image: string, i: number) => i !== index
+                                )
+                                setInitialImages(newImages)
+                                initialData?.editInitialImages(newImages)
+                              }}
+                            >
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-2 w-2"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  strokeWidth={5}
+                                  d="M6 18L18 6M6 6l12 12"
+                                />
+                              </svg>
+                            </button>
+                          </div>
+                        )
+                      )
+                    })}
                 </div>
               </>
             )}
