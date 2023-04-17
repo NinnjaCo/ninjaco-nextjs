6fcc08005b06b6c5124edab6b412e8cb9fc744c3
Murf-y <charbelfayad64@gmail.com>
2023-04-17 19:08:29 +0300

    feat: create blockly component

---
+++ components/blockly/blockly.tsx
@@ -0,0 +1,74 @@
+import 'blockly/blocks'
+import { BlocklyOptions } from 'blockly'
+import { load, save } from '@/blockly/serialization'
+import Blockly from 'blockly'
+import React, { useCallback, useEffect, useRef } from 'react'
+
+interface blocksDefinition {
+  [key: string]: any
+}
+interface BlocklyBoardProps {
+  blocklyOptions: BlocklyOptions
+  blocksDefinitions?: blocksDefinition
+  codeGenerator: Blockly.CodeGenerator
+}
+
+const BlocklyBoard = (props: BlocklyBoardProps) => {
+  const blocklyDiv = useRef<HTMLDivElement>(null)
+  const primaryWorkspace = useRef<Blockly.WorkspaceSvg>()
+  const [loaded, setLoaded] = React.useState(false)
+
+  const generateCode = useCallback(() => {
+    const code = props.codeGenerator.workspaceToCode(primaryWorkspace.current)
+    console.log(code)
+  }, [props])
+
+  useEffect(() => {
+    const { toolbox, ...rest } = props.blocklyOptions
+
+    if (blocklyDiv.current) {
+      if (loaded && primaryWorkspace.current?.rendered) {
+        return
+      }
+      // Start the workspace
+      if (props.blocksDefinitions) Blockly.common.defineBlocks(props.blocksDefinitions)
+      const workspace = Blockly.inject(blocklyDiv.current, {
+        toolbox: toolbox,
+        ...rest,
+      })
+      primaryWorkspace.current = workspace as Blockly.WorkspaceSvg
+
+      load(workspace)
+      if (!workspace) {
+        throw new Error('Could not load workspace')
+      }
+
+      setLoaded(true)
+      workspace.addChangeListener((e) => {
+        // UI events are things like scrolling, zooming, etc.
+        // No need to save after one of these.
+        if (e.isUiEvent) return
+        save(workspace)
+      })
+      // Whenever the workspace changes meaningfully, run the code again.
+      workspace.addChangeListener((e) => {
+        // Don't run the code when the workspace finishes loading; we're
+        // already running it once when the application starts.
+        // Don't run the code during drags; we might have invalid state.
+        if (e.isUiEvent || e.type == Blockly.Events.FINISHED_LOADING || workspace.isDragging()) {
+          return
+        }
+        // generateCode()
+      })
+    }
+  }, [primaryWorkspace, blocklyDiv, props, loaded, generateCode])
+
+  return (
+    <div className="w-full h-full flex flex-col">
+      <div ref={blocklyDiv} className="w-full h-full " />
+      <button onClick={generateCode}>Convert</button>
+    </div>
+  )
+}
+
+export default BlocklyBoard
