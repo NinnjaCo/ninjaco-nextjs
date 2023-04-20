cd2ef9cf0ec9c28a4bde08fb485e10f63f5e120c
Murf-y <charbelfayad64@gmail.com>
2023-04-20 14:16:43 +0300

    feat: update UserPlayGame model

--- models/crud/game-enrollment.model.ts
+++ models/crud/game-enrollment.model.ts
@@ -1,19 +1,21 @@
-import { GameProgress } from "./game-Progress.model"
-import { Game } from "./game.model"
-import { User } from "./user.model"
+import { Game } from './game.model'
+import { GameProgress } from './game-Progress.model'
+import { User } from './user.model'
 
 export interface UserPlayGame {
-    game: Game
-    user: User
-    gameProgress: GameProgress
-    completed: boolean
-    startedAt: string
+  _id: string
+  game: Game
+  user: User
+  gameProgress: GameProgress
+  completed: boolean
+  startedAt: string
+  createdAt: string
+  updatedAt: string
 }
-  
+
 export interface UserPlayGameRequest {
-    userId: string
-    gameId: string
-    progress: string
-    completed: boolean
+  userId: string
+  gameId: string
+  progress?: string
+  completed?: boolean
 }
-  
