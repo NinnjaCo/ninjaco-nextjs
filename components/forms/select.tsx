8fd8dc1d8f46aeb51c50077b57e62eb57f39740a
Murf-y <charbelfayad64@gmail.com>
2023-04-21 22:18:14 +0300

    feat: dont allow editing the course type

--- components/forms/select.tsx
+++ components/forms/select.tsx
@@ -16,6 +16,7 @@ interface SelectProps {
   isRequired?: boolean
   rootClassName?: string
   placeholder?: string
+  disabled?: boolean
 }
 
 export default function Select({
@@ -27,6 +28,7 @@ export default function Select({
   isRequired,
   placeholder,
   rootClassName,
+  disabled,
 }: SelectProps) {
   return (
     <div className={clsx('flex flex-col gap-2 z-20', rootClassName)}>
@@ -47,7 +49,15 @@ export default function Select({
               }}
             >
               <div className="relative mt-1">
-                <Listbox.Button className="block w-full px-4 py-2 text-brand-700 placeholder-brand-400 border border-brand-300 rounded-md focus:outline-2 focus:ring-brand-500 focus:border-brand-500 sm:text-sm">
+                <Listbox.Button
+                  aria-disabled={disabled}
+                  className={clsx(
+                    'block w-full px-4 py-2 text-brand-700 placeholder-brand-400 border border-brand-300 rounded-md focus:outline-2 focus:ring-brand-500 focus:border-brand-500 sm:text-sm',
+                    {
+                      'bg-brand-50 cursor-not-allowed': disabled,
+                    }
+                  )}
+                >
                   <span className="block truncate text-left text-brand">
                     {field.value ?? placeholder}
                   </span>
@@ -63,39 +73,41 @@ export default function Select({
                   leaveFrom="opacity-100"
                   leaveTo="opacity-0"
                 >
-                  <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-brand ring-opacity-5 focus:outline-none sm:text-sm">
-                    {selectList.map((option, optionIdx) => (
-                      <Listbox.Option
-                        key={optionIdx}
-                        className={({ active }) =>
-                          clsx(
-                            'relative cursor-default select-none m-2 rounded-lg py-2 pl-10 pr-4',
-                            {
-                              'bg-brand-100 text-brand-700': active,
-                            }
-                          )
-                        }
-                        value={option}
-                      >
-                        {({ selected }) => (
-                          <>
-                            <span
-                              className={clsx('block truncate font-normal', {
-                                'font-medium': selected,
-                              })}
-                            >
-                              {option}
-                            </span>
-                            {selected ? (
-                              <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-brand">
-                                <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                  {disabled ? null : (
+                    <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-brand ring-opacity-5 focus:outline-none sm:text-sm">
+                      {selectList.map((option, optionIdx) => (
+                        <Listbox.Option
+                          key={optionIdx}
+                          className={({ active }) =>
+                            clsx(
+                              'relative cursor-default select-none m-2 rounded-lg py-2 pl-10 pr-4',
+                              {
+                                'bg-brand-100 text-brand-700': active,
+                              }
+                            )
+                          }
+                          value={option}
+                        >
+                          {({ selected }) => (
+                            <>
+                              <span
+                                className={clsx('block truncate font-normal', {
+                                  'font-medium': selected,
+                                })}
+                              >
+                                {option}
                               </span>
-                            ) : null}
-                          </>
-                        )}
-                      </Listbox.Option>
-                    ))}
-                  </Listbox.Options>
+                              {selected ? (
+                                <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-brand">
+                                  <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                </span>
+                              ) : null}
+                            </>
+                          )}
+                        </Listbox.Option>
+                      ))}
+                    </Listbox.Options>
+                  )}
                 </Transition>
               </div>
             </Listbox>
