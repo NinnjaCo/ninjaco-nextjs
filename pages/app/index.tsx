670488dbcdae631b30df0c840453863f2364b31a
tony123bs <toni123bousleiman@gmail.com>
2023-04-21 15:17:47 +0300

    feat: add creatorMeno for user courses page

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -1,12 +1,21 @@
+import { Course } from '@/models/crud/course.model'
+import { CourseEnrollment } from '@/models/crud/course-enrollment.model'
 import { CourseEnrollmentAPI } from '@/utils/api/courseEnrollment/course-enrollment.api'
 import { GameEnrollmentAPI } from '@/utils/api/game-enrollment/game-enrollment.api'
+import { User } from '@/models/crud'
 import { authOptions } from '../api/auth/[...nextauth]'
 import { getServerSession } from 'next-auth'
 import CreatorMenu from '@/components/creator/creatorMenu'
 import Head from 'next/head'
 import React from 'react'
 
-export default function MainApp() {
+export default function MainApp({
+  user,
+  courses,
+}: {
+  user: User
+  courses: (CourseEnrollment | Course)[]
+}) {
   return (
     <>
       <Head>
