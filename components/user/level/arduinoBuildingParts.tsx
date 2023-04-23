1a2ccc24f696ffd58f1b8f34efd6ff52e847e296
Raghidkh <raghidkhoury2@gmail.com>
2023-04-24 01:30:16 +0300

    feat: add translation to user aruido level components

--- components/user/level/arduinoBuildingParts.tsx
+++ components/user/level/arduinoBuildingParts.tsx
@@ -1,11 +1,13 @@
 import MultiImageCarousel from './multiImageCarousel'
 import React from 'react'
+import useTranslation from '@/hooks/useTranslation'
 
 const ArduinoBuildingParts = ({ buildingPartsImages }: { buildingPartsImages: string[] }) => {
+  const t = useTranslation()
   return (
     <div className="m-24">
       <div className="w-full text-start text-brand text-2xl font-bold my-12">
-        Building Parts that Might be Need :{' '}
+        {t.User.arduinoLevel.buildingPartMayNeed}{' '}
       </div>
       <MultiImageCarousel images={buildingPartsImages} />
     </div>
