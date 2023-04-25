c7680d2f605bec91b07f7ec91a94ce2cbd18ed43
Murf-y <charbelfayad64@gmail.com>
2023-04-25 16:47:34 +0300

    fix: btn cta style

--- pages/creator/games/index.tsx
+++ pages/creator/games/index.tsx
@@ -33,10 +33,7 @@ export default function Home({ user, games }: { user: User; games: Game[] }) {
                 {t.Creator.games.viewGames.title}
               </div>
               <div className="text-brand-700 font-semibold">
-                <Link
-                  className="btn btn-secondary bg-secondary rounded-xl text-brand-700 border-brand-700 hover:bg-secondary-800 py-2 h-fit"
-                  href="/creator/games/create"
-                >
+                <Link className="btn btn-cta" href="/creator/games/create">
                   {t.Creator.games.viewGames.createGame}
                 </Link>
               </div>
