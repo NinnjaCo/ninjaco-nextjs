04bfe95f00fc8d6a13fc8362b37feb9d7f391d1d
Raghidkh <raghidkhoury2@gmail.com>
2023-04-25 19:38:57 +0300

    feat: add feedback model

---
+++ models/crud/feedback.model.ts
@@ -0,0 +1,23 @@
+import { Course } from './course.model'
+import { Level } from './level.model'
+import { Mission } from './mission.model'
+import { User } from 'next-auth'
+
+export interface Feedback {
+  _id: string
+  user: User
+  course: Course
+  mission: Mission
+  level: Level
+  rating: number
+  message: string
+}
+
+export interface FeedbackRequest {
+  userId: string
+  courseId: string
+  missionId: string
+  levelId: string
+  rating: number
+  message: string
+}
