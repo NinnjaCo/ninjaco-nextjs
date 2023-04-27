fa65feb65abe9f3bb01e6297442307aed8c76c07
Murf-y <charbelfayad64@gmail.com>
2023-04-27 15:23:40 +0300

    feat: create arduinoToolbox

---
+++ blockly/toolbox/arduino.ts
@@ -0,0 +1,195 @@
+import '../blocks/arduino/base'
+import '../blocks/arduino/text'
+import '../generetors/arduino/index'
+
+import { ToolboxDefinition } from 'blockly/core/utils/toolbox'
+export const arduinoToolbox: ToolboxDefinition = {
+  kind: 'categoryToolbox',
+  contents: [
+    {
+      kind: 'category',
+      name: 'Logic',
+      colour: '#5b80a5',
+      contents: [
+        {
+          kind: 'block',
+          type: 'controls_if',
+        },
+        {
+          kind: 'block',
+          type: 'logic_compare',
+        },
+        {
+          kind: 'block',
+          type: 'logic_operation',
+        },
+        {
+          kind: 'block',
+          type: 'logic_negate',
+        },
+        {
+          kind: 'block',
+          type: 'logic_null',
+        },
+      ],
+    },
+    {
+      kind: 'category',
+      name: 'Control',
+      contents: [
+        {
+          kind: 'block',
+          type: 'base_delay',
+        },
+        {
+          kind: 'block',
+          type: 'controls_for',
+        },
+        {
+          kind: 'block',
+          type: 'controls_whileUntil',
+        },
+      ],
+    },
+    {
+      kind: 'category',
+      name: 'Math',
+      contents: [
+        {
+          kind: 'block',
+          type: 'math_number',
+        },
+        {
+          kind: 'block',
+          type: 'math_arithmetic',
+        },
+        {
+          kind: 'block',
+          type: 'base_map',
+        },
+      ],
+    },
+    {
+      kind: 'category',
+      name: 'Text',
+      contents: [
+        {
+          kind: 'block',
+          type: 'text',
+        },
+      ],
+    },
+    {
+      kind: 'category',
+      name: 'Variables',
+      custom: 'VARIABLE',
+    },
+    {
+      kind: 'category',
+      name: 'Functions',
+      custom: 'PROCEDURE',
+    },
+    {
+      kind: 'sep',
+    },
+    {
+      kind: 'category',
+      name: 'Input/Output',
+      contents: [
+        {
+          kind: 'block',
+          type: 'inout_highlow',
+        },
+        {
+          kind: 'block',
+          type: 'inout_digital_write',
+        },
+        {
+          kind: 'block',
+          type: 'inout_digital_read',
+        },
+        {
+          kind: 'block',
+          type: 'inout_analog_write',
+        },
+        {
+          kind: 'block',
+          type: 'inout_analog_read',
+        },
+        {
+          kind: 'block',
+          type: 'serial_print',
+        },
+        {
+          kind: 'block',
+          type: 'inout_tone',
+        },
+        {
+          kind: 'block',
+          type: 'inout_notone',
+        },
+        {
+          kind: 'block',
+          type: 'inout_buildin_led',
+        },
+      ],
+    },
+    {
+      kind: 'category',
+      name: 'Servo',
+      contents: [
+        {
+          kind: 'block',
+          type: 'servo_move',
+        },
+        {
+          kind: 'block',
+          type: 'servo_read_degrees',
+        },
+      ],
+    },
+  ],
+}
+
+//     <category name="Grove Analog">
+//       <block type="grove_rotary_angle"></block>
+//       <block type="grove_temporature_sensor"></block>
+//       <block type="grove_sound_sensor"></block>
+//       <block type="grove_thumb_joystick"></block>
+//     </category>
+//     <category name="Grove">
+//       <block type="grove_led"></block>
+//       <block type="grove_button"></block>
+//       <block type="grove_relay"></block>
+//       <block type="grove_tilt_switch"></block>
+//       <block type="grove_piezo_buzzer"></block>
+//       <block type="grove_pir_motion_sensor"></block>
+//       <block type="grove_line_finder"></block>
+//       <block type="grove_rgb_led"></block>
+//       <block type="grove_ultrasonic_ranger"></block>
+//     </category>
+//     <category name="Grove LCD">
+//       <block type="grove_serial_lcd_print">
+//         <value name="TEXT">
+//           <block type="text">
+//             <field name="TEXT"></field>
+//           </block>
+//         </value>
+//         <value name="TEXT2">
+//           <block type="text">
+//             <field name="TEXT"></field>
+//           </block>
+//         </value>
+//         <value name="DELAY_TIME">
+//           <block type="math_number">
+//             <field name="NUM">1000</field>
+//           </block>
+//         </value>
+//       </block>
+//       <block type="grove_serial_lcd_power"></block>
+//       <block type="grove_serial_lcd_effect"></block>
+//     </category>
+//     <category name="Grove Motor">
+//       <block type="grove_motor_shield"></block>
+//     </category>
+//   </xml>
