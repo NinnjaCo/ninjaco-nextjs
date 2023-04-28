e29f87e6bc2eb8c2c3374a04aef5d0febb5d5a4a
Murf-y <charbelfayad64@gmail.com>
2023-04-28 22:00:09 +0300

    feat: add lcd blocks

--- blockly/blocks/arduino/base.js
+++ blockly/blocks/arduino/base.js
@@ -211,3 +211,102 @@ Blockly.Blocks['serial_print'] = {
     this.setTooltip('Prints data to the console/serial port as human-readable ASCII text.')
   },
 }
+
+Blockly.Blocks['grove_serial_lcd_print'] = {
+  helpUrl:
+    'http://www.seeedstudio.com/wiki/index.php?title=GROVE_-_Starter_Bundle_V1.0b#Serial_LCD',
+  init: function () {
+    this.setColour(IO_HUE)
+    this.appendDummyInput()
+      .appendField('Serial LCD')
+      .appendField(
+        new Blockly.FieldImage(
+          'https://statics3.seeedstudio.com/images/product/Serial%20LCD.jpg',
+          64,
+          64
+        )
+      )
+      .appendField('PIN#')
+      .appendField(new Blockly.FieldDropdown(profile.default.digital), 'PIN')
+    this.appendValueInput('TEXT', 'String')
+      .setCheck('String')
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField('print line1')
+    this.appendValueInput('TEXT2', 'String')
+      .setCheck('String')
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField('print line2')
+    this.appendValueInput('DELAY_TIME', 'Number')
+      .setCheck('Number')
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField('Delay')
+    this.setPreviousStatement(true, null)
+    this.setNextStatement(true, null)
+    this.setTooltip('print text on an 16 character by 2 line LCD.')
+  },
+}
+
+//grove lcd power on/off
+Blockly.Blocks['grove_serial_lcd_power'] = {
+  helpUrl: 'http://www.seeedstudio.com/wiki/index.php?title=GROVE_-_Starter_Bundle_V1.0b#LED',
+  init: function () {
+    this.setColour(IO_HUE)
+    this.appendDummyInput()
+      .appendField('Serial LCD')
+      .appendField(
+        new Blockly.FieldImage(
+          'https://statics3.seeedstudio.com/images/product/Serial%20LCD.jpg',
+          64,
+          64
+        )
+      )
+      .appendField('PIN#')
+      .appendField(new Blockly.FieldDropdown(profile.default.digital), 'PIN')
+    this.appendDummyInput()
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField('Power')
+      .appendField(
+        new Blockly.FieldDropdown([
+          ['ON', 'ON'],
+          ['OFF', 'OFF'],
+        ]),
+        'STAT'
+      )
+    this.setPreviousStatement(true, null)
+    this.setNextStatement(true, null)
+    this.setTooltip('Turn LCD power on/off')
+  },
+}
+
+//scroll left/right/no scroll/blink/noblink
+Blockly.Blocks['grove_serial_lcd_effect'] = {
+  helpUrl: 'http://www.seeedstudio.com/wiki/index.php?title=GROVE_-_Starter_Bundle_V1.0b#LED',
+  init: function () {
+    this.setColour(IO_HUE)
+    this.appendDummyInput()
+      .appendField('Serial LCD')
+      .appendField(
+        new Blockly.FieldImage(
+          'https://statics3.seeedstudio.com/images/product/Serial%20LCD.jpg',
+          64,
+          64
+        )
+      )
+      .appendField('PIN#')
+      .appendField(new Blockly.FieldDropdown(profile.default.digital), 'PIN')
+    this.appendDummyInput()
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField('Effect')
+      .appendField(
+        new Blockly.FieldDropdown([
+          ['Scroll Left', 'LEFT'],
+          ['Scroll Right', 'RIGHT'],
+          ['Scroll Auto', 'AUTO'],
+        ]),
+        'STAT'
+      )
+    this.setPreviousStatement(true, null)
+    this.setNextStatement(true, null)
+    this.setTooltip('Turn LCD power on/off')
+  },
+}
